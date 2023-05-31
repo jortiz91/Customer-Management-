38acff5d15002866442ff05f6829d3d440953765
Jonathan Ortiz-Ryals <Jonathanortizryals@gmail.com>
2023-05-31 16:53:31 -0400

    install mongoose

--- contact/app.js
+++ contact/app.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
+const connectDB = require('./server/config/database')
 
 const app = express()
 const port = 3000 || process.abort.env.PORT
+
+connectDB()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
