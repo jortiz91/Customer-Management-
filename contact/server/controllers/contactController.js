b5c559d7a89a425d956eb619c683cc7c210b2008
Jonathan Ortiz-Ryals <Jonathanortizryals@gmail.com>
2023-06-01 14:44:12 -0400

    Add timestamp

--- contact/server/controllers/contactController.js
+++ contact/server/controllers/contactController.js
@@ -1,3 +1,6 @@
+const Contact = require('../models/contact')
+const mongoose = require('mongoose')
+
 exports.homepage = async (req, res) => {
   const locals = {
     title: 'About',
@@ -14,4 +17,22 @@ exports.addContact = async (req, res) => {
   }
 
   res.render('contact/add', locals)
+}
+
+exports.postContact = async (req, res) => {
+  const newContact = new Contact({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    notes: req.body.notes,
+    tel: req.body.tel,
+    email: req.body.email
+  })
+
+  try {
+    await Contact.create(newContact)
+
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
 }
--- contact/server/routes/contact.js
+++ contact/server/routes/contact.js
@@ -4,5 +4,6 @@ const contactController = require('../controllers/contactController')
 
 router.get('/', contactController.homepage)
 router.get('/add', contactController.addContact)
+router.post('/add', contactController.postContact)
 
 module.exports = router
